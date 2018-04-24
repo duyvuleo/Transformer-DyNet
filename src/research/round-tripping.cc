22f17b2e8c3fc416656424f053cac598c715942b
Vu Cong Duy Hoang <duyvuleo@gmail.com>
2018-04-24 13:33:03 +0200

    [WIP] - round tripping: load data+models

--- src/research/round-tripping.cc
+++ src/research/round-tripping.cc
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <sys/stat.h>
 
 #include <boost/program_options/parsers.hpp>
 #include <boost/program_options/variables_map.hpp>
@@ -23,6 +24,7 @@
 
 using namespace std;
 using namespace dynet;
+using namespace transformer;
 using namespace boost::program_options;
 
 unsigned MAX_EPOCH = 10;
@@ -35,7 +37,7 @@ int main_body(variables_map vm);
 typedef WordIdSentences MonoData;
 
 // read the data
-MonoData read_mono_data(const string &filename, const dynet::Dict& d);
+MonoData read_mono_data(dynet::Dict& d, const string &filepath, unsigned max_seq_len=0, unsigned load_percent=100);
 
 // main round tripping function
 void run_round_tripping(transformer::TransformerModel& s2t_mod, transformer::TransformerModel& t2t_mod
@@ -54,13 +56,15 @@ int main(int argc, char** argv) {
 		("help", "print help message")
 		("config,c", value<string>(), "config file specifying additional command line options")
 		//-----------------------------------------		
-		("model-path-s2t,p", value<std::string>()->default_value("."), "pre-trained path for the source-to-target transformer model will be loaded from this folder")
-		("model-path-t2s,p", value<std::string>()->default_value("."), "pre-trained path for the target-to-source transformer model will be loaded from this folder")
-		("model-path-s,p", value<std::string>()->default_value("."), "pre-trained path for the target language model will be loaded from this folder")
-		("model-path-t,p", value<std::string>()->default_value("."), "pre-trained path for the source language model will be loaded from this folder")
-		("dev,d", value<string>(), "file containing development parallel sentences, with "
+		("model-path-s2t", value<std::string>()->default_value("."), "pre-trained path for the source-to-target transformer model will be loaded from this folder")
+		("model-path-t2s", value<std::string>()->default_value("."), "pre-trained path for the target-to-source transformer model will be loaded from this folder")
+		("model-path-s", value<std::string>()->default_value("."), "pre-trained path for the target language model will be loaded from this folder")
+		("model-path-t", value<std::string>()->default_value("."), "pre-trained path for the source language model will be loaded from this folder")
+		("devel,d", value<string>(), "file containing development parallel sentences, with "
 			"each line consisting of source ||| target.")
-		//-----------------------------------------
+		("max-seq-len", value<unsigned>()->default_value(0), "limit the sequence length loaded from mono data; none by default")
+		("mono-load-percent", value<unsigned>()->default_value(100), "limit the use of <num> percent of sequences in monolingual data; full by default")
+		//-----------------------------------------	
 		("K", value<unsigned>()->default_value(10), "the K value for sampling K-best translations from transformer models")
 		("beam_size", value<unsigned>()->default_value(5), "the beam size of beam search decoding")
 		("alpha", value<float>()->default_value(0.5f), "the alpha hyper-parameter for balancing the rewards")
@@ -98,21 +102,134 @@ int main(int argc, char** argv) {
 	float gamma_2 = vm["gamma_2"].as<unsigned>();
 	MAX_EPOCH = vm["epoch"].as<unsigned>();
 	DEV_ROUND = vm["dev_round"].as<unsigned>();
+
+	//--- load models
+	// Transformer Model recipes
+	std::vector<std::shared_ptr<transformer::TransformerModel>> v_tf_models;
+	transformer::SentinelMarkers sm;// sentinel markers
+
+	std::vector<std::string> model_paths({vm["model-path-s2t"].as<std::string>(), vm["model-path-t2s"].as<std::string>()});		
+	for (auto& model_path : model_paths){
+		std::string config_file = model_path + "/model.config";// configuration file path
+		struct stat sb;
+		if (stat(config_file.c_str(), &sb) == 0 && S_ISREG(sb.st_mode)){// check existence	
+			// load vocabulary from file(s)
+			dynet::Dict sd, td;// vocabularies
+			std::string vocab_file = model_path + "/" + "src-tgt.joint-vocab";
+			if (stat(vocab_file.c_str(), &sb) == 0 && S_ISREG(sb.st_mode)){
+				load_vocab(model_path + "/" + "src-tgt.joint-vocab", sd);
+				td = sd;
+			}
+			else{
+				std::string src_vocab_file = model_path + "/" + "src.vocab";
+				std::string tgt_vocab_file = model_path + "/" + "tgt.vocab";
+				load_vocabs(src_vocab_file, tgt_vocab_file, sd, td);
+			}
+
+			sd.freeze();
+			td.freeze();
+			
+			sm._kSRC_SOS = sd.convert("<s>");
+			sm._kSRC_EOS = sd.convert("</s>");
+			sm._kTGT_SOS = td.convert("<s>");
+			sm._kTGT_EOS = td.convert("</s>");
+
+			// read model configuration
+			transformer::TransformerConfig tfc;
+			ifstream inpf_cfg(config_file);
+			assert(inpf_cfg);
+		
+			std::string line;
+			getline(inpf_cfg, line);
+			std::stringstream ss(line);
+			tfc._src_vocab_size = sd.size();
+			tfc._tgt_vocab_size = td.size();
+			tfc._sm = sm;
+			ss >> tfc._num_units >> tfc._nheads >> tfc._nlayers >> tfc._n_ff_units_factor
+			   >> tfc._encoder_emb_dropout_rate >> tfc._encoder_sublayer_dropout_rate >> tfc._decoder_emb_dropout_rate >> tfc._decoder_sublayer_dropout_rate >> tfc._attention_dropout_rate >> tfc._ff_dropout_rate 
+			   >> tfc._use_label_smoothing >> tfc._label_smoothing_weight
+			   >> tfc._position_encoding >> tfc._position_encoding_flag >> tfc._max_length
+			   >> tfc._attention_type
+			   >> tfc._ffl_activation_type
+			   >> tfc._shared_embeddings
+			   >> tfc._use_hybrid_model;
+			tfc._is_training = true;
+			tfc._use_dropout = true;
+
+			// load models
+			v_tf_models.push_back(std::shared_ptr<transformer::TransformerModel>());
+			v_tf_models.back().reset(new transformer::TransformerModel(tfc, sd, td));
+			std::string model_file = model_path + "/model.params";
+			if (stat(model_file.c_str(), &sb) == 0 && S_ISREG(sb.st_mode))
+			{
+				cerr << endl << "Loading pre-trained model from file: " << model_file << "..." << endl;
+				v_tf_models.back()->initialise_params_from_file(model_file);// load pre-trained model (for incremental training)
+			}
+			cerr << "Count of model parameters: " << v_tf_models.back()->get_model_parameters().parameter_count() << endl;
+		}
+		else TRANSFORMER_RUNTIME_ASSERT("Failed to load model(s) from: " + std::string(model_path) + "!");
+	}
+
+	// Transformer LModel recipes
+	std::vector<std::shared_ptr<transformer::TransformerLModel>> v_tf_lmodels;
+	model_paths.clear();
+	model_paths.insert(model_paths.begin(), {vm["model-path-s"].as<std::string>(), vm["model-path-t"].as<std::string>()});
+	for (auto& model_path : model_paths){
+		std::string config_file = model_path + "/model.config";// configuration file path
+		struct stat sb;
+		if (stat(config_file.c_str(), &sb) == 0 && S_ISREG(sb.st_mode)){// check existence	
+			// load vocabulary from files
+			dynet::Dict d;
+			std::string vocab_file = model_path + "/" + "vocab";
+			load_vocab(vocab_file, d);
+
+			// load config file
+			transformer::TransformerConfig tfc;
+			tfc._tgt_vocab_size = d.size();
+			tfc._sm = sm;
+
+			ifstream inpf_cfg(config_file);
+			assert(inpf_cfg);
+			std::string line;
+			getline(inpf_cfg, line);
+			std::stringstream ss(line);
+			ss >> tfc._num_units >> tfc._nheads >> tfc._nlayers >> tfc._n_ff_units_factor
+			   >> tfc._decoder_emb_dropout_rate >> tfc._decoder_sublayer_dropout_rate >> tfc._attention_dropout_rate >> tfc._ff_dropout_rate 
+			   >> tfc._use_label_smoothing >> tfc._label_smoothing_weight
+			   >> tfc._position_encoding >> tfc._max_length
+			   >> tfc._attention_type
+			   >> tfc._ffl_activation_type
+			   >> tfc._use_hybrid_model;		
+			tfc._is_training = false;// language models will be fixed?
+			tfc._use_dropout = false;
+
+			// load models
+			v_tf_lmodels.push_back(std::shared_ptr<transformer::TransformerLModel>());
+			v_tf_lmodels.back().reset(new transformer::TransformerLModel(tfc, d));
+			std::string model_file = model_path + "/model.params";
+			if (stat(model_file.c_str(), &sb) == 0 && S_ISREG(sb.st_mode))
+			{
+				cerr << endl << "Loading pre-trained model from file: " << model_file << "..." << endl;
+				v_tf_lmodels.back()->initialise_params_from_file(model_file);// load pre-trained model (for incremental training)
+			}
+			cerr << "Count of model parameters: " << v_tf_lmodels.back()->get_model_parameters().parameter_count() << endl;
+		}
+	}
 	
 	//--- load data
-	dynet::Dict sdict, tdict;
-	
 	// monolingual corpora
 	// Assume that these monolingual corpora use the same vocabularies with parallel corpus	used for training.
 	MonoData mono_cor_s, mono_cor_t;
 	cerr << "Reading monolingual source data from " << vm["mono_s"].as<string>() << "...\n";
-	mono_cor_s = read_mono_data(vm["mono_s"].as<string>(), sdict);
+	mono_cor_s = read_mono_data(v_tf_lmodels[0]->get_dict(), vm["mono_s"].as<string>(), vm["max-seq-len"].as<unsigned>(), vm["mono-load-percent"].as<unsigned>());
 	cerr << "Reading monolingual target data from " << vm["mono_t"].as<string>() << "...\n";
-	mono_cor_t = read_mono_data(vm["mono_t"].as<string>(), tdict);
-	
-	//--- load models
-	// FIXME
+	mono_cor_t = read_mono_data(v_tf_lmodels[1]->get_dict(), vm["mono_t"].as<string>(), vm["max-seq-len"].as<unsigned>(), vm["mono-load-percent"].as<unsigned>());
 
+	// development parallel data
+	WordIdCorpus devel_cor, devel_cor_rev;// integer-converted dev parallel data
+	cerr << "Reading dev parallel data from " << vm["devel"].as<std::string>() << "...\n";
+	devel_cor = read_corpus(vm["devel"].as<std::string>(), &v_tf_models[0]->get_source_dict(), &v_tf_models[0]->get_target_dict(), false/*for development*/);
+	devel_cor_rev = read_corpus(vm["devel"].as<std::string>(), &v_tf_models[1]->get_source_dict(), &v_tf_models[1]->get_target_dict(), false/*for development*/);		
 
 	//--- execute dual-learning
 	// FIXME
@@ -130,11 +247,26 @@ void run_round_tripping(transformer::TransformerModel& s2t_mod, transformer::Tra
 	// FIXME
 }
 
-MonoData read_mono_data(const string &filename, const dynet::Dict& d)
+MonoData read_mono_data(dynet::Dict& d, const string &filepath, unsigned max_seq_len, unsigned load_percent)
 {
-	MonoData corpus;
-	// FIXME
-	return corpus;
+	MonoData mono = read_corpus(filepath, &d, true, max_seq_len);
+	
+	// limit the percent of training data to be used
+	if (load_percent < 100 
+		&& load_percent > 0)
+	{
+		cerr << "Only use " << load_percent << "% of " << mono.size() << " instances: ";
+		unsigned int rev_pos = load_percent * mono.size() / 100;
+		mono.erase(mono.begin() + rev_pos, mono.end());
+		cerr << mono.size() << " instances remaining!" << endl;
+	}
+	else if (load_percent != 100){
+		cerr << "Invalid --mono-load-percent <num> used. <num> must be (0,100]" << endl;
+		cerr << "All data will be used!" << endl;
+		return mono;// return full
+	}
+
+	return mono;
 }
 
 
